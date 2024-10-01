153ac71d66cef12edd2c0b91c7236a9736fbdd5f
Dehvon Curtis <dcurtis@datto.com>
2024-10-01 15:26:07 -0600

    Arrays and Structs

--- Solidity-Smart-Contract-Development/Section-1-Simple-Storage/SimpleStorage.sol
+++ Solidity-Smart-Contract-Development/Section-1-Simple-Storage/SimpleStorage.sol
@@ -4,18 +4,32 @@ pragma solidity ^0.8.18;
 contract SimpleStorage {
     // favoriteNumber gets initialized to `0` if no value
 
-    uint256 public favoriteNumber; // 0
-    // uint256[] listOfFavoriteNumber;
+    uint256 myFavoriteNumber; // 0
+    uint256[] listOfFavoriteNumber; // [0,12,55]
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-        retrieve();
+        myFavoriteNumber = _favoriteNumber;
     }
 
     // view, pure
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+
+    // dynamic array (grows and shrinks)
+    Person[] public listOfPeople;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+    }
+
 
 }
 
