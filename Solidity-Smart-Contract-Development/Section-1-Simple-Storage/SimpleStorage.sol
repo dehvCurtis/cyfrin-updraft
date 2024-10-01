29bb2740ad5461c0f2410a7dfbb57c07499a481c
Dehvon Curtis <dcurtis@datto.com>
2024-10-01 15:07:57 -0600

    commit

--- Solidity-Smart-Contract-Development/Section-1-Simple-Storage/SimpleStorage.sol
+++ Solidity-Smart-Contract-Development/Section-1-Simple-Storage/SimpleStorage.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.18;
 contract SimpleStorage {
     // favoriteNumber gets initialized to `0` if no value
 
-    uint256 favoriteNumber;
-    uint256[] listOfFavoriteNumber;
+    uint256 public favoriteNumber; // 0
+    // uint256[] listOfFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+        retrieve();
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
     }
 
 }
