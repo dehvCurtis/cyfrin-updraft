2445836e576dedbb640815001747f4400c49c357
Dehvon Curtis <dcurtis@datto.com>
2024-10-01 14:55:31 -0600

    commit

--- Solidity-Smart-Contract-Development/Section-1-Simple-Storage/SimpleStorage.sol
+++ Solidity-Smart-Contract-Development/Section-1-Simple-Storage/SimpleStorage.sol
@@ -1,1 +1,15 @@
-//
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+    // favoriteNumber gets initialized to `0` if no value
+
+    uint256 favoriteNumber;
+    uint256[] listOfFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+}
+
