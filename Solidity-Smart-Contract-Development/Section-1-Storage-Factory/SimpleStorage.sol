67492deb159c29c3b3b78e1424f198eaca537a95
Dehvon Curtis <dcurtis@datto.com>
2024-10-07 10:18:41 -0600

    commit

---
+++ Solidity-Smart-Contract-Development/Section-1-Storage-Factory/SimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+    // favoriteNumber gets initialized to `0` if no value
+
+    uint256 myFavoriteNumber; // 0
+    uint256[] listOfFavoriteNumber; // [0,12,55]
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
+    }
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+
+    // dynamic array (grows and shrinks)
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavNumber;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavNumber[_name] = _favoriteNumber;
+    }
+}
